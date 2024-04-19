b6c462f76aee3bd388a757aa9c6c7a5694d6cad9
Teemu Kallio <Teemu.Kallio3@metropolia.fi>
2024-04-19 10:17:40 +0300

    remove redundant validators from profile

--- page/profile/profile.js
+++ page/profile/profile.js
@@ -58,15 +58,4 @@ const login = () => {
     .addEventListener('click', registration);
 };
 
-const validateEmail = (email) => {
-  const regex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  return regex.test(email);
-};
-
-const validatePassword = (password) => {
-  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-  return regex.test(password);
-};
-
 login();
